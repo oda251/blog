9cb272eb18b7b64068610e0722d46a2a72f0260a
oda251 <pgwyaueu@gmail.com>
2024-08-13 02:40:36 +0900

    v1 fix bug

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,2 @@
-/// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-type Runtime = import("@astrojs/cloudflare").DirectoryRuntime<Env>;
-declare namespace App {
-	interface Locals extends Runtime {}
-}
+/// <reference path="../.astro/types.d.ts" />
