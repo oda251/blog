c68c933326b4960e5c01c6c850a01af0695d82e5
oda251 <pgwyaueu@gmail.com>
2024-09-19 07:42:54 +0900

    [fixed]little bag in rendering tweet

--- app/src/components/Tweet/TweetView.tsx
+++ app/src/components/Tweet/TweetView.tsx
@@ -10,7 +10,7 @@ interface TweetProps {
 const TweetView = forwardRef<HTMLDivElement, TweetProps>((props, ref) => {
   return (
     <div ref={ref} className={`p-4 shadow-md rounded-lg ${props.className}`}>
-      <p className="tweet-content text-md text-gray-200 mb-2 break-words">{props.tweet.content}</p>
+      <p className="tweet-content text-md text-gray-200 mb-2 break-words whitespace-pre-wrap">{props.tweet.content}</p>
       <p className="tweet-author text-sm text-gray-400 mb-1 break-words">By {props.tweet.author === "" ? "名無しさん" : props.tweet.author}</p>
       <p className="tweet-created-at text-xs text-gray-400">{props.tweet.created_at}</p>
     </div>
